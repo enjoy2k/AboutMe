954658a7b8a8676cdf099ba9fdd348d06e195eaf
enjoy2k <Enjoy2k@mail.ru>
2022-11-15 15:26:00 +0300

    Промежуточный результат1

--- AboutMe/ViewControllers/LoginScreenViewController.swift
+++ AboutMe/ViewControllers/LoginScreenViewController.swift
@@ -29,7 +29,7 @@ class LoginScreenViewController: UIViewController {
                 welcomeVC.userName = daniil.name
                 welcomeVC.userSurname = daniil.surname
             } else if let navigationVC = viewController as? UINavigationController {
-                let aboutUserVC = navigationVC.topViewController as! TransferViewController
+                let _ = navigationVC.topViewController as! TransferViewController
                 
             }
             }
